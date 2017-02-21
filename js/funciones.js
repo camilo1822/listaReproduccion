4bfd2c9423f50b7e83c5cf4f8faee05818991d67
juan.arboleda <juan.arboleda@pragma.com.co>
2017-02-21 17:30:16 -0500

    reconoce fin

--- js/funciones.js
+++ js/funciones.js
@@ -5,6 +5,8 @@ __jquery(document).ready(function() {
 
 function onReadyPortal(__jquery) {
 	 if (__jquery("#principal").length) {
+        var videos=[];
+        var id;
            reproducir();
            lista();
         }
@@ -12,15 +14,61 @@ function onReadyPortal(__jquery) {
 
 function reproducir(){
     __jquery("#ahora").attr('src','https://www.youtube.com/embed/aKuivabiOns?autoplay=1');
+     __jquery("#ahora1").attr('data','https://www.youtube.com/embed/aKuivabiOns?autoplay=1');
     if ( __jquery("#ahora").paused){
         alert("d");
     } 
 }
 
 function lista(){
-    var videos = ['https://www.youtube.com/embed/adBxyO1H73M','https://www.youtube.com/embed/adBxyO1H73M','https://www.youtube.com/embed/adBxyO1H73M'];
-    alert(videos.length)
+    videos = ['https://www.youtube.com/watch?time_continue=17&v=aKuivabiOns','https://www.youtube.com/watch?time_continue=17&v=aKuivabiOns','https://www.youtube.com/watch?time_continue=17&v=aKuivabiOns'];
     for (var i = 0; i < videos.length; i++) { 
          __jquery("#secundarios").append("<iframe src='"+videos[i]+"'></iframe>");
+    }
+}
+
+function obtenerIdUrl(name,url) {
+    try {
+        var regexS = "[\\?&]" + name + "=([^&#]*)";
+        var regex = new RegExp(regexS);   
+        var results = regex.exec(url);    
+        if (results == null) {
+            return "";
+        } else {
+            return results[1];
+        }
+    } catch (excepcion) {
+        dispararExcepcion(excepcion, "interna.min.js", "obtenerIdUrl()");
+    }
+}
+
+var player;
+id='aKuivabiOns';
+function onYouTubePlayerAPIReady() {
+    alert("acxa");
+    player = new YT.Player('player', {
+        height: '390',
+        width: '640',
+        videoId: id,
+        events: {
+        'onReady': onPlayerReady,
+        'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+
+
+    // autoplay video
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+    // when video ends
+function onPlayerStateChange(event) {        
+    if(event.data === 0) {          
+        var x = obtenerIdUrl("v",videos[1]);
+        id='GaI8dMnauGk';
+        onYouTubePlayerAPIReady();
     }
 }
